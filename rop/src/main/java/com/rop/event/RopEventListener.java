c75e1204e9e9791b87a6be8e3a629d56ce78c4dc
Long Yuan <794334212@qq.com>
2015-05-29 17:36:27 +0800

    Signed-off-by: Long Yuan <794334212@qq.com>

--- rop/src/main/java/com/rop/event/RopEventListener.java
+++ rop/src/main/java/com/rop/event/RopEventListener.java
@@ -1,16 +1,55 @@
-//package com.rop.event;
-//
-//import org.springframework.context.ApplicationListener;
-//
-///**
-// * <pre>
-// *    监听所有Rop框架的事件
-// * </pre>
-// *
-// * @author 筱龙缘
-// * @version 1.0
-// */
-//public interface RopEventListener<E extends RopEvent> extends ApplicationListener<E> {
-//
-//}
-//
+package com.rop.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.SmartApplicationListener;
+
+/**
+ * <pre>
+ *    监听所有Rop框架的事件
+ * </pre>
+ *
+ * @author 筱龙缘
+ * @version 1.0
+ */
+public class RopEventListener<E extends RopEvent> implements SmartApplicationListener {
+
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        return eventType == E;
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> sourceType) {
+        return false;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+
+//    @Override
+//    public boolean supportsEventType(final Class<? extends ApplicationEvent> eventType) {
+//        return eventType == ContentEvent.class;
+//    }
+//    @Override
+//    public boolean supportsSourceType(final Class<?> sourceType) {
+//        return sourceType == String.class;
+//    }
+//    @Override
+//    public void onApplicationEvent(final ApplicationEvent event) {
+//        System.out.println("王五在孙六之前收到新的内容：" + event.getSource());
+//    }
+//    @Override
+//    public int getOrder() {
+//        return 1;
+//    }
+}
+
